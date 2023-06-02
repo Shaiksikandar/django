1edf9d31cbd6cfa6721a30280724c9f8ef7e4c3a
Admin <admin@gmail.com>
2023-06-02 12:00:00 +0530

    two

--- shaik/views.py
+++ shaik/views.py
@@ -8,10 +8,13 @@ def home(r):
 def contact(r):
 	return render(r,"contact.html")
 
-def books(r):
-	return render(r,"books.html")
+def about(r):
+	return render(r,"about.html")
+	
+def services(r):
+	return render(r,"services.html")
 
-def about(req):
+def books(req):
 	con = sqlite3.connect("db.sqlite3")
 	cur = con.cursor()
 	res = cur.execute("SELECT name from sqlite_master")
@@ -25,9 +28,7 @@ def about(req):
 		 PRIMARY KEY("id" AUTOINCREMENT)
 		 );""")
 
-
 	if req.POST.get("add_book"):
-
 		book = req.POST.get("book")
 		year = req.POST.get("year")
 		price = req.POST.get("price")
@@ -39,22 +40,30 @@ def about(req):
 		id = req.POST.get("id")
 		cur.execute("DELETE FROM books WHERE id = ?" , id)
 		con.commit()
-
 	books = cur.execute("SELECT * FROM books").fetchall()
-	
 	x = {
 		"books" : books
 	}
+	return render(req, "books.html", x )
 
-	
+def book_edit(r):
+	con = sqlite3.connect(db.sqlite3)
+	cur = con.cursor()
+	id = r.POST.get("id")
 
-    
+	if r.POST.get("save"):
+		book = r.POST.get("book")
+		year = r.POST.get("year")
+		price = r.POST.get("price")
 
+		print(book)
 
+		cur.execute("SELECT * FROM books WHERE id=?" , id ) .fetchone()
 
-
-	return render(req, "about.html", x )
-
-
-def services(r):
-	return render(r,"services.html")
+		x = {
+			"book" : book[1],
+			"year" : year[1],
+			"price" : price[1],
+		}
+		
+		# return render("r, book_edit.html", x)
